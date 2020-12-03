80152333b5880bef1fee838b0b5bf5d542dc0ffb
Kartik Sehgal <Kartiksehgal3@gmail.com>
2020-12-04 01:05:59 +0530

    4

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const cors = require("cors");
 const emailRouter = require("./routers/emailRouter");
 
 const app = express();
+app.set("port", process.env.PORT || 5000);
 
 app
   .use(cors())
   .use(bodyParser.json())
   .use(bodyParser.urlencoded())
   .use("/email", emailRouter)
-  .listen(process.env.PORT, "0.0.0.0");
+  .listen(app.get("port"), function () {
+    console.log(
+      "App is running, server is listening on port ",
+      app.get("port")
+    );
+  });
